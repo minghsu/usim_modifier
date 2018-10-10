161f2081bce43df2dd05a78617c59e56a004d0ab
Ming <hsu.chihming@gmail.com>
2018-10-10 15:10:03 +0800

    - re-factor "status" plugin

--- model/plugin/plugins/status.py
+++ model/plugin/plugins/status.py
@@ -6,6 +6,12 @@ import os
 
 from model.plugin.plugins.base_plugin import base_plugin
 
+execute_list = ["iccid",
+                "imsi",
+                "mccmnc",
+                "spn",
+                "gid"]
+
 
 class status(base_plugin):
     def __init__(self):
@@ -24,15 +30,12 @@ class status(base_plugin):
     def execute(self, arg_connection, arg_parameter=""):
         self.__logging.debug("execute()")
 
-        ret_content = super(status, self).execute_plugin(
-            "iccid", arg_connection)
-        ret_content += ("\n" +
-                        super(status, self).execute_plugin("imsi", arg_connection))
-        ret_content += ("\n" +
-                        super(status, self).execute_plugin("mccmnc", arg_connection))
-        ret_content += ("\n" +
-                        super(status, self).execute_plugin("gid", arg_connection))
-        ret_content += ("\n" +
-                        super(status, self).execute_plugin("spn", arg_connection))
+        ret_content = ""
+        for plugin in execute_list:
+            if ret_content != "":
+                ret_content += "\n"
+
+            ret_content += super(status, self).execute_plugin(
+                plugin, arg_connection)
 
         return ret_content
